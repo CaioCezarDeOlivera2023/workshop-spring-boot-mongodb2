f162c9e21060fc1600fd845cbe73352ec4bb97e5
Caio Cezar <caio25mgmail.com>
2025-01-20 16:43:26 -0300

    Cirando entity Post com User aninhado

--- worshopmongo/src/main/java/com/caiocezar/workshopmongo/resource/UserResource.java
+++ worshopmongo/src/main/java/com/caiocezar/workshopmongo/resource/UserResource.java
@@ -25,33 +25,33 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UserDTO>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {//aqui ira retornar uma lista de usuarios
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {// aqui ira buscar os usuario, através do id
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {//aqui ira inserir novos usuario através de uma requisição
 		User obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();//aqui ira retornar uma resposta vazia com o cod 201
+		return ResponseEntity.created(uri).build();//aqui ira retornar uma resposta vazia com o cod 201, após a requisição
 		
 	}
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id) {
+	public ResponseEntity<Void> delete(@PathVariable String id) {//aqui ira deleter um usuaria através de uma reuisição
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {//auqi ira fazer uma atualização de um usuario atrvés de uma requisição
 		User obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
