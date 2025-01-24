87a9afad1a0729693354b665bc8c27470db1d812
Caio Cezar <caio25mgmail.com>
2025-01-24 12:16:34 -0300

    Consulta simples com @query

--- worshopmongo/src/main/java/com/caiocezar/workshopmongo/repository/PostRepository.java
+++ worshopmongo/src/main/java/com/caiocezar/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.caiocezar.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.caiocezar.workshopmongo.domain.Post;
@@ -10,6 +11,10 @@ import com.caiocezar.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title' : { $regex : ?0, $options : 'i' } }")
+	List<Post> findByTitle(String text);
+	
+	
 	List<Post> findBytitleContainingIgnoreCase(String text);
 	
 }
